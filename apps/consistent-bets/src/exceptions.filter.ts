f12511644ed7b56fe9a4929edb5a90600a6307a2
Brenno Rodrigues <brennopereira6@gmail.com>
2023-08-21 21:02:14 -0300

    docs: added todo list on readme

---
+++ apps/consistent-bets/src/exceptions.filter.ts
@@ -0,0 +1,40 @@
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus, Logger } from '@nestjs/common';
+import { Request, Response } from 'express'
+
+@Catch(HttpException)
+export class AllExceptionsFilter implements ExceptionFilter {
+    private readonly logger = new Logger(AllExceptionsFilter.name);
+
+    catch(exception: HttpException, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse<Response>();
+        const request = ctx.getRequest<Request>();
+        const status = exception.getStatus();
+
+        this.logger.error({
+            method: request.method,
+            host: request.hostname,
+            path: request.path,
+            ip: request.ip,
+            message: exception.message,
+            statusCode: status,
+            stacktrace: exception.stack,
+            timestamp: new Date().toISOString(),
+        });
+
+        if(status === 500)
+            return response
+                .status(500)
+                .json({
+                    status,
+                    message: 'Unexpected error has occurred'
+                });
+
+        response
+            .status(status)
+            .json({
+                status,
+                message: exception.message
+            });
+    }
+}
--- apps/consistent-bets/src/main.ts
+++ apps/consistent-bets/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new AllExceptionsFilter())
   await app.listen(3001);
 }
 
